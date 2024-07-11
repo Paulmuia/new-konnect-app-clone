d804f85e8bb212b941d1aabe0cade75a2cb1cd4a
Paulmuia <paulmuia7511@gmail.com>
2024-07-11 09:05:23 +0300

    added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:konnect_app/pages/Authentication/login.dart';
-import 'package:konnect_app/pages/Authentication/verification_page.dart';
 
 
 
