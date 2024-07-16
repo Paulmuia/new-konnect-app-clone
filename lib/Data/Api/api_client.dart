5981e3a500890b4bcc1ff493d629daab533f5414
Paulmuia <paulmuia7511@gmail.com>
2024-07-16 09:47:08 +0300

    adding folders

---
+++ lib/Data/Api/api_client.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title:const Text('Search Page', style: TextStyle(color: Colors.green,fontWeight: FontWeight.w700,fontStyle: FontStyle.italic),),
+      ),
+      body: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
