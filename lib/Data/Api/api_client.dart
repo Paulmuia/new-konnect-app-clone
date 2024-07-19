52aee75063da28bedb34f64d77ea5543386fcef0
Paulmuia <paulmuia7511@gmail.com>
2024-07-19 13:17:33 +0300

    modifing files

--- lib/Data/Api/api_client.dart
+++ lib/Data/Api/api_client.dart
@@ -1,4 +1,5 @@
 import 'package:animated_rating_stars/animated_rating_stars.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:konnect_app/widgets/icon_texts.dart';
 
@@ -15,92 +16,61 @@ class _ApiClientState extends State<ApiClient> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   backgroundColor: Colors.green,
-      //   title: const Text(
-      //     'Home Page',
-      //     style: TextStyle(
-      //       color: Colors.white,
-      //       fontSize: 25,
-      //       fontWeight: FontWeight.w800,
-      //     ),
-      //   ),
-      //   leading: const Padding(
-      //     padding: EdgeInsets.only(left: 20),
-      //     child: CircleAvatar(
-      //       backgroundColor: Colors.white,
-      //       radius: 5,
-      //       child: Icon(
-      //         Icons.person,
-      //         size: 20,
-      //         color: Colors.green,
-      //       ),
-      //     ),
-      //   ),
-      //   actions: const [
-      //     Icon(
-      //       Icons.search,
-      //       color: Colors.white,
-      //       size: 30,
-      //     ),
-      //     SizedBox(
-      //       width: 5,
-      //     ),
-      //     Icon(
-      //       Icons.shopping_cart_outlined,
-      //       color: Colors.white,
-      //       size: 30,
-      //     ),
-      //     SizedBox(
-      //       width: 5,
-      //     ),
-      //     Icon(Icons.more_vert_outlined, color: Colors.white, size: 30),
-      //     SizedBox(
-      //       width: 5,
-      //     ),
-      //   ],
-      // ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   const Column(
                     children: [
-                      Text('Kenya',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w700),),
+                      Text(
+                        'Kenya',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700),
+                      ),
                       Row(
                         children: [
-                          Text('Nairobi',style: TextStyle(fontSize: 15),),
-                           SizedBox(width: 10,),
+                          Text(
+                            'Nairobi',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
                           Icon(Icons.arrow_drop_down)
                         ],
                       )
                     ],
                   ),
-                  
                   Container(
                     height: 40,
                     width: 40,
                     decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.green
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.green),
+                    child: const Center(
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
                     ),
-                    child:const Center(child: Icon(Icons.search,color: Colors.white,),),
                   )
                 ],
               ),
             ),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
             Container(
               color: Colors.white,
               width: double.maxFinite,
               height: 260,
-              
               child: PageView.builder(
                   itemCount: 10,
                   controller: pageController,
@@ -242,88 +212,117 @@ class _ApiClientState extends State<ApiClient> {
                     );
                   }),
             ),
-            const SizedBox(height: 20,),
-            const Text('   Recommended For you',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w700),),
-            const SizedBox(height: 15,),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+                child: DotsIndicator(
+                    decorator: const DotsDecorator(
+                      activeColor: Colors.amber,
+                      color: Colors.grey,
+                      activeSize: Size(15, 15),
+                      size: Size(10, 10),
+                    ),
+                    dotsCount: 5)),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              '   Recommended For you',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
             Expanded(
               child: ListView.builder(
-                itemCount: 10,
-                //physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context,index){
-                return Container(
-                  height: 130,
-                  margin: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
-                  width: double.maxFinite,
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
-                        height: 120,
-                        width: 120,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(image: AssetImage('assets/images/food_14.png'),fit: BoxFit.cover),
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          height: 120,
-                          padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
-                          color: Colors.white,
-                          child: const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                  ' Nutritious food meal in kenya',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      overflow: TextOverflow.ellipsis,
-                                      fontSize: 23,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                SizedBox(height: 10,),
-                                 Text(
-                                  '  With Kenyan characteristic',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                                 SizedBox(
-                                  height: 10,
-                                ),
-                                 Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    IconText(
-                                        icon: Icons.circle,
-                                        color: Colors.amber,
-                                        size: 20,
-                                        text: 'Normal'),
-                                    IconText(
-                                        icon: Icons.location_on,
-                                        color: Colors.blueGrey,
-                                        size: 20,
-                                        text: '32 KM'),
-                                    IconText(
-                                        icon: Icons.timer,
-                                        color: Colors.green,
-                                        size: 20,
-                                        text: '45 min')
-                                  ],
-                                )
-                            ],
+                  itemCount: 10,
+                  //physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 130,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 5, vertical: 5),
+                      width: double.maxFinite,
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 5),
+                            height: 120,
+                            width: 120,
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/images/food_14.png'),
+                                    fit: BoxFit.cover),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                color: Colors.white),
                           ),
-                        ),
-                      )
-                      
-                    ],
-                  ),
-                );
-              }),
+                          Expanded(
+                            child: Container(
+                              height: 120,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5, vertical: 5),
+                              color: Colors.white,
+                              child: const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    ' Nutritious food meal in kenya',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        overflow: TextOverflow.ellipsis,
+                                        fontSize: 23,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    '  With Kenyan characteristic',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      IconText(
+                                          icon: Icons.circle,
+                                          color: Colors.amber,
+                                          size: 20,
+                                          text: 'Normal'),
+                                      IconText(
+                                          icon: Icons.location_on,
+                                          color: Colors.blueGrey,
+                                          size: 20,
+                                          text: '32 KM'),
+                                      IconText(
+                                          icon: Icons.timer,
+                                          color: Colors.green,
+                                          size: 20,
+                                          text: '45 min')
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
             )
           ],
         ),
